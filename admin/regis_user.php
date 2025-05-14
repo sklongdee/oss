1fc8112ba346b962e698cfc29d0778321fd2a521
suttipong klongdee <stp.suttipong@gmail.com>
2025-05-14 14:22:31 +0700

    first commit

--- admin/regis_user.php
+++ admin/regis_user.php
@@ -1,15 +1,15 @@
-<form>
+<form action="regis_user_verify.php" method="POST">
   <div class="mb-3">
-    <label for="username" class="form-label">username</label>
-    <input type="text" class="form-control" id="username">
+    <label for="username" class="form-label">Username</label>
+    <input type="text" class="form-control" id="username" name="username">
   </div>
   <div class="mb-3">
     <label for="password" class="form-label">Password</label>
-    <input type="password" class="form-control" id="password">
+    <input type="password" class="form-control" id="password" name="password">
   </div>
   <div class="mb-3">
     <label for="role" class="form-label">ระดับผู้ใช้งาน</label>
-    <select class="form-select" id="role">
+    <select class="form-select" id="role" name="role">
         <option selected>เลือกระดับผู้ใช้งาน</option>
         <option value="admin">admin</option>
         <option value="user">user</option>
@@ -17,7 +17,7 @@
   </div>
   <div class="mb-3">
     <label for="_name" class="form-label">ชื่อ-นามสกุล</label>
-    <input type="text" class="form-control" id="_name">
+    <input type="text" class="form-control" id="_name" name="_name">
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
